0da348cf1b5bd124ca7a3cb15c6bd34cf20a44dd
christopher <chrisolivaresdev@gmail.com>
2023-06-08 23:16:08 -0400

    cambios

--- qr-app/src/app/services/g-qr.service.ts
+++ qr-app/src/app/services/g-qr.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { promotion } from '../interfaces/promotion.interfaces';
 
